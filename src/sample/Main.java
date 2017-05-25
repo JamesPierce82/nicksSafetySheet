2bc70c3785a858aa10dc7a9985d4630162c57642
James Pierce <james.pierce82@stclairconnect.ca>
2017-05-25 16:22:56 -0400

    Added missing label

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -38,6 +38,7 @@ public class Main extends Application {
         Label commentsLabel = new Label("\nComments");
 
         // Row labels
+        Label inspectionResultLabel = new Label("INSPECTION RESULT");
         Label section1Label = new Label("SECTION 1. POWERTRAIN");
         Label section2Label = new Label("SECTION 2. SUSPENSION");
         Label section3Label = new Label("SECTION 3. BRAKE SYSTEMS");
@@ -75,11 +76,9 @@ public class Main extends Application {
         Label fuelLevelLabel = new Label("Fuel Level");
 
         // Add children to the GridPane.
-        // Row 0
         gPane.add(OSILabel,0, 0 , 3, 1);
         gPane.add(autoClinicLabel, 3, 0, 3, 1);
 
-        // Row 1
         gPane.add(legendLabel, 0,1);
         gPane.add(okLabel, 2,1);
         gPane.add(repairRequiredLabel, 3,1);
@@ -87,110 +86,77 @@ public class Main extends Application {
         gPane.add(suggestedPerformanceLabel, 5,1);
         gPane.add(commentsLabel, 6,1);
 
-        // Row 2
-        gPane.add(section1Label, 0, 2);
+        gPane.add(inspectionResultLabel, 0, 2);
 
-        // Row 3
-        gPane.add(section2Label, 0, 3);
+        gPane.add(section1Label, 0, 3);
 
-        // Row 4
-        gPane.add(section3Label, 0, 4);
+        gPane.add(section2Label, 0, 4);
 
-        // Row 5
-        gPane.add(leftFrontPadsLabel, 0, 5);
+        gPane.add(section3Label, 0, 5);
 
-        // Row 6
-        gPane.add(rightFrontPadsLabel, 0, 6);
+        gPane.add(leftFrontPadsLabel, 0, 6);
 
-        // Row 7
-        gPane.add(leftRearPadsLabel, 0, 7);
+        gPane.add(rightFrontPadsLabel, 0, 7);
 
-        // Row 8
-        gPane.add(rightRearPadsLabel, 0 , 8);
+        gPane.add(leftRearPadsLabel, 0, 8);
 
-        // Row 9
-        gPane.add(frontRotorsDrumsLabel, 0 , 9);
+        gPane.add(rightRearPadsLabel, 0 , 9);
 
-        // Row 10
-        gPane.add(rearRotorsDrumsLabel, 0 ,10);
+        gPane.add(frontRotorsDrumsLabel, 0 , 10);
 
-        // Row 11
-        gPane.add(frontBrakeShoesLabel, 0 ,11);
+        gPane.add(rearRotorsDrumsLabel, 0 ,11);
 
-        // Row 12
-        gPane.add(rearBrakeShoesLabel, 0 ,12);
+        gPane.add(frontBrakeShoesLabel, 0 ,12);
 
-        // Row 13
-        gPane.add(section4Label, 0, 13);
+        gPane.add(rearBrakeShoesLabel, 0 ,13);
 
-        // Row 14
-        gPane.add(section5Label, 0 , 14);
+        gPane.add(section4Label, 0, 14);
 
-        // Row 15
-        gPane.add(section6label, 0 , 15);
+        gPane.add(section5Label, 0 , 15);
 
-        // Row 16
-        gPane.add(section7Label, 0 , 16);
+        gPane.add(section6label, 0 , 16);
 
-        // Row 17
-        gPane.add(section8Label, 0 , 17);
+        gPane.add(section7Label, 0 , 17);
 
-        // Row 18
-        gPane.add(windowTintLabel, 0 ,18);
+        gPane.add(section8Label, 0 , 18);
 
-        // Row 19
-        gPane.add(section9Label, 0 , 19);
+        gPane.add(windowTintLabel, 0 ,19);
 
-        // Row 20
-        gPane.add(frontTreadDepthLabel, 0 ,20);
+        gPane.add(section9Label, 0 , 20);
 
-        // Row 21
-        gPane.add(rearTreadDepthLabel, 0 ,21);
+        gPane.add(frontTreadDepthLabel, 0 ,21);
 
-        // Row 22
-        gPane.add(tirePressureLabel, 0 ,22);
+        gPane.add(rearTreadDepthLabel, 0 ,22);
 
-        // Row 23
-        gPane.add(frontLeftLabel, 0 ,23);
+        gPane.add(tirePressureLabel, 0 ,23);
 
-        // Row 24
-        gPane.add(frontRightLabel, 0, 24);
+        gPane.add(frontLeftLabel, 0 ,24);
 
-        // Row 25
-        gPane.add(rearLeftLabel, 0 , 25);
+        gPane.add(frontRightLabel, 0, 25);
 
-        // Row 26
-        gPane.add(rearRightLabel, 0 , 26);
+        gPane.add(rearLeftLabel, 0 , 26);
 
-        // Row 27
-        gPane.add(section10Label, 0 , 27);
+        gPane.add(rearRightLabel, 0 , 27);
 
-        // Row 28
-        gPane.add(section11Label, 0 , 28);
+        gPane.add(section10Label, 0 , 28);
 
-        // Row 29
-        gPane.add(warningLightsLabel, 0 , 29);
+        gPane.add(section11Label, 0 , 29);
 
-        // Row 30
-        gPane.add(ABSLabel, 0, 30);
+        gPane.add(warningLightsLabel, 0 , 30);
 
-        // Row 31
-        gPane.add(TPMSLabel, 0, 31);
+        gPane.add(ABSLabel, 0, 31);
 
-        // Row 32
-        gPane.add(checkEngineLabel, 0 , 32);
+        gPane.add(TPMSLabel, 0, 32);
 
-        // Row 33
-        gPane.add(airBagsLabel, 0 , 33);
+        gPane.add(checkEngineLabel, 0 , 33);
 
-        // Row 34
-        gPane.add(electronicStabilityControlLabel, 0 , 34);
+        gPane.add(airBagsLabel, 0 , 34);
 
-        // Row 35
-        gPane.add(otherLightsLabel, 0 , 35);
+        gPane.add(electronicStabilityControlLabel, 0 , 35);
 
-        // Row 36
-        gPane.add(fuelLevelLabel, 0 , 36);
+        gPane.add(otherLightsLabel, 0 , 36);
+
+        gPane.add(fuelLevelLabel, 0 , 37);
 
         // Create the scene, and show the window
         Scene scene = new Scene(sPane, 1024, 768);
