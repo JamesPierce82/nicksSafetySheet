1bde145f62f79844dca23e45df85397ef86e4aaa
James Pierce <james.pierce82@stclairconnect.ca>
2017-05-25 15:48:02 -0400

    Added labels
    * Added a bunch of the required labels for the headers as well as about half to 3/4 of the labels for the rows.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -14,7 +14,72 @@ public class Main extends Application {
         // Create the Gridpane for displaying the form itself
         GridPane gPane = new GridPane();
 
+        gPane.setHgap(15);
+        gPane.setVgap(10);
 
+        gPane.setGridLinesVisible(true);
+
+        // Create children to be placed in gPane
+        Label OSILabel = new Label("Ontario Safety Inspection");
+        Label autoClinicLabel = new Label("Nick's Auto Clinic");
+
+        // Header labels
+        Label legendLabel = new Label("***Legend***\nOK = PASS\nREPAIR REQUIRED = FAIL");
+        Label okLabel = new Label("OK\n  ");
+        Label repairRequiredLabel = new Label("REPAIR\nREQUIRED");
+        Label suggestedPreventiveLabel = new Label("SUGGESTED\nPREVENTIVE");
+        Label suggestedPerformanceLabel = new Label("SUGGESTED\nPERFORMANCE");
+        Label commentsLabel = new Label("\nComments");
+
+        // Row labels
+        Label section1Label = new Label("SECTION 1. POWERTRAIN");
+        Label section2Label = new Label("SECTION 2. SUSPENSION");
+        Label section3Label = new Label("SECTION 3. BRAKE SYSTEMS");
+        Label leftFrontPadsLabel = new Label("Left Front Pads(mm)");
+        Label rightFrontPadsLabel = new Label("RIght Front Pads(mm)");
+        Label leftRearPadsLabel = new Label("Left Rear Pads(mm)");
+        Label rightRearPadsLabel = new Label("Right Rear Pads(mm)");
+        Label frontRotorsDrumsLabel = new Label("Front Rotors or Drums(mm)");
+        Label rearRotorsDrumsLabel = new Label("Rear Rotors or Drums(mm)");
+        Label frontBrakeShoesLabel = new Label("Front Brake Shoes(mm)");
+        Label rearBrakeShoesLabel = new Label("Rear Brake Shoes(mm)");
+        Label section4Label = new Label("SECTION 4. STEERING");
+        Label section5Label = new Label("SECTION 5. INSTRUMENTS AND AUXILIARY EQUIPMENT");
+        Label section6label = new Label("SECTION 6. LAMPS");
+        Label section7Label  = new Label("SECTION 7. ELECTRICAL SYSTEMS");
+        Label section8Label  = new Label("SECTION 8. BODY");
+        Label windowTintLabel  = new Label("Window Tint");
+        Label section9Label = new Label("SECTION 9. TIRE AND WHEEL");
+        Label frontTreadDepthLabel  = new Label("Front Tread Depth");
+        Label readTreadDepthLabel  = new Label("Rear Tread Depth");
+        Label   = new Label("");
+        Label  = new Label("");
+        Label  = new Label("");
+        Label  = new Label("");
+        Label  = new Label("");
+        Label  = new Label("");
+        Label  = new Label("");
+        Label  = new Label("");
+        Label  = new Label("");
+        Label  = new Label("");
+        Label  = new Label("");
+        Label  = new Label("");
+        Label  = new Label("");
+        Label  = new Label("");
+        Label  = new Label("");
+
+        // Add children to the GridPane.
+        // Row 0
+        gPane.add(OSILabel,0, 0 , 3, 1);
+        gPane.add(autoClinicLabel, 3, 0, 3, 1);
+
+        // Row 1
+        gPane.add(legendLabel, 0,1);
+        gPane.add(okLabel, 2,1);
+        gPane.add(repairRequiredLabel, 3,1);
+        gPane.add(suggestedPreventiveLabel, 4,1);
+        gPane.add(suggestedPerformanceLabel, 5,1);
+        gPane.add(commentsLabel, 6,1);
 
         // Create the scene, and show the window
         Scene scene = new Scene(gPane, 1024, 768);
